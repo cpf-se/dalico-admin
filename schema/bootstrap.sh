1d2996aba59c90c777c6e15fe46a40c3dc745103
Christian Andersson <christian.ld.andersson@gmail.com>
2011-05-24 07:12:30 +0200

    fixed bootstrapper

--- schema/bootstrap.sh
+++ schema/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "drop database dalico ; create database dalico owner dalico template template0 encoding 'UTF8' lc_collate 'sv_SE.UTF8' ;" | psql template1
-rm .*.done
+echo "drop database if exists dalico ; create database dalico owner dalico template template0 encoding 'UTF8' lc_collate 'sv_SE.UTF8' ;" | psql template1
+rm -f .*.done
 
