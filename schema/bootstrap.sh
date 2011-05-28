d2ef113812de764a9ccde4bbab8e1da31a92f7c5
Christian Andersson <christian.ld.andersson@gmail.com>
2011-05-28 02:55:45 +0200

    PostgreSQL on dalby lacks Swedish collation

--- schema/bootstrap.sh
+++ schema/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "drop database if exists dalico ; create database dalico owner dalico template template0 encoding 'UTF8' lc_collate 'sv_SE.UTF8' ;" | psql template1
+echo "drop database if exists dalico ; create database dalico owner dalico template template0 encoding 'UTF8' ;" | psql template1
 rm -f .*.done
 
