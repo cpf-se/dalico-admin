b5218b15dff8651383f8af16ade65b01c12530a0
Christian Andersson <christian@cpf.se>
2011-05-30 17:35:16 +0200

    initial IVP version

---
+++ application/views/ivpform.php
@@ -0,0 +1,11 @@
+
+<?php $this->load->view('header', array('userdata' => $userdata['userdata']));?>
+
+<h1>Interventionsprotokoll</h1>
+<?php echo "<p><strong>$patient</strong>&nbsp;/&nbsp;$date</p>\n";?>
+<?php echo validation_errors();?>
+
+<?php // initialize fields
+
+?>
+
