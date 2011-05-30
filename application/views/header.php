9ea2e918a6c265fc663488ea89f0ffcf48137e83
Christian Andersson <christian.ld.andersson@gmail.com>
2011-05-30 07:00:56 +0200

    added home link to header

--- application/views/header.php
+++ application/views/header.php
@@ -10,5 +10,5 @@
 		<div id="head"><img src="/logo_head_dalby.jpg" /></div>
 		<div id="topright">
 		<strong><?=$userdata['firstname']?>&nbsp;<?=$userdata['lastname']?></strong>&nbsp;&lt;<?=$userdata['email']?>&gt;<br />
-		<a href="/auth/logout">Logga ut</a>&nbsp;&nbsp;&nbsp;<a href="/auth/change_password">Byt lösenord</a>&nbsp;&nbsp;&nbsp;<a href="/auth/change_email">Byt emailadress</a>&nbsp;&nbsp;&nbsp;<a href="/bugs">Buggar</a>
+		<a href="/">Hem</a>&nbsp;&nbsp;&nbsp;<a href="/auth/logout">Logga ut</a>&nbsp;&nbsp;&nbsp;<a href="/auth/change_password">Byt lösenord</a>&nbsp;&nbsp;&nbsp;<a href="/auth/change_email">Byt emailadress</a>&nbsp;&nbsp;&nbsp;<a href="/bugs">Buggar</a>
 		</div>
