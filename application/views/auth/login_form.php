ae1eee5a8ba62d96dbba77ec36bc39662f4e68bd
Christian Andersson <christian@cpf.se>
2011-06-13 14:27:04 +0200

    tag on login_form

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -84,7 +84,7 @@ $captcha = array(
 		<td colspan="3">
 			<!--?php echo form_checkbox($remember); ?-->
 			<!--?php echo form_label('Remember me', $remember['id']); ?-->
-			<?php echo anchor('/auth/forgot_password/', 'Forgot password'); ?>
+			<?php echo anchor('/auth/forgot_password/', 'Forgot password / New user'); ?>
 			<!--?php if ($this->config->item('allow_registration', 'tank_auth')) echo anchor('/auth/register/', 'Register'); ?-->
 		</td>
 	</tr>
